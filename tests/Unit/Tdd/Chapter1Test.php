cd865325ee1b16f0376e707c262fb9deb7e7bc99
soyeon <ryumirr1@naver.com>
2025-02-27 11:09:53 +0900

    코멘트아웃만 추가

--- tests/Unit/Tdd/Chapter1Test.php
+++ tests/Unit/Tdd/Chapter1Test.php
@@ -6,11 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 class Chapter1Test extends TestCase
 {
+    // public function test_異なる通話単位とたす場合(string $currency_unit_name): void
+    // {
+    //     $usa = config('currency_unit.usa');
+    //     $swiss = config('currency_unit.swiss');
+    // }
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
+    // public function test_that_true_is_true(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
 }
